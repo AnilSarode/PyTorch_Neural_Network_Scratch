db3463af80cacffff380f7cb18205a3a0f192779
AnilSarode <sarodeanil35@gmail.com>
2024-07-08 22:36:29 +0530

    Bug resolved with plot_decision_boundary_PyTorch

--- PyTorch_Planar_Utils.py
+++ PyTorch_Planar_Utils.py
@@ -48,6 +48,23 @@ def plot_decision_boundary(model, X, y):
     plt.scatter(X[0, :].numpy(), X[1, :].numpy(), c=y.numpy(), cmap=plt.cm.Spectral)
 
 
+
+def plot_decision_boundary_PyTorch(model, X, y):
+    # Set min and max values and give it some padding
+    x_min, x_max = X[0, :].min().item() - 1, X[0, :].max().item() + 1
+    y_min, y_max = X[1, :].min().item() - 1, X[1, :].max().item() + 1
+    h = 0.01
+    # Generate a grid of points with distance h between them
+    xx, yy = torch.meshgrid(torch.arange(x_min, x_max, h), torch.arange(y_min, y_max, h), indexing='ij')
+    # Predict the function value for the whole grid
+    xy_tensor = torch.cat((xx.reshape(-1, 1), yy.reshape(-1, 1)), dim=1)
+    Z = model(xy_tensor).detach().cpu().numpy() # important step
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.ylabel('x2')
+    plt.xlabel('x1')
+    plt.scatter(X[0, :].numpy(), X[1, :].numpy(), c=y.numpy(), cmap=plt.cm.Spectral)
 def sigmoid(x):
     """
     Compute the sigmoid of x
